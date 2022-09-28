c28aace5c3c2c17c946a59a966de68a1ad9d65d7
Manishdowellresearch <manish@dowellresearch.in>
2022-09-28 15:50:43 +0530

    ok

--- webapp_test/scaleapi/views.py
+++ webapp_test/scaleapi/views.py
@@ -3,12 +3,16 @@ from django.http import HttpResponse ,JsonResponse
 from function.dowellconnection import dowellconnection
 from function.event import get_event_id
 from django.views.decorators.csrf import csrf_exempt
+import json
+
 
 @csrf_exempt
 def scale(request):
     if request.method == "POST":
         data= request.POST.get('data')
+        a=type(data)
         return JsonResponse({
+            "type":a,
             "data": data 
             })
 
@@ -16,9 +20,10 @@ def scale(request):
 def scaleapi(request):
     if request.method == "POST":
         scale_data = request.POST.get('scale_data')
+        scale=json.loads(scale_data)
         field ={
             "eventId":get_event_id(),
-            "scale_data": scale_data
+            "scale_data": scale
         }
         inserted_id= dowellconnection("dowellscale","dowellscale","scale_reports","scale_reports","1094","ABCDE","insert",field)
         return JsonResponse({
