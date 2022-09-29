46c1c6c700dbe5c4a759552beaf631c1fc689581
Manishdowellresearch <manish@dowellresearch.in>
2022-09-29 10:57:29 +0530

    ok

--- webapp_test/scaleapi/views.py
+++ webapp_test/scaleapi/views.py
@@ -10,20 +10,35 @@ import json
 def scale(request):
     if request.method == "POST":
         data= request.POST.get('data')
-        a=type(data)
         return JsonResponse({
-            "type":a,
             "data": data 
             })
 
 @csrf_exempt
 def scaleapi(request):
     if request.method == "POST":
-        scale_data = request.POST.get('scale_data')
-        scale=json.loads(scale_data)
+        orientation = request.POST.get("orientation")
+        numberrating= request.POST.get("numberrating")
+        scalecolor= request.POST.get("scalecolor")
+        roundcolor= request.POST.get("roundcolor")
+        fontcolor= request.POST.get("fontcolor")
+        fomat= request.POST.get("fomat")
+        time= request.POST.get("time")
+        template_name = request.POST.get("template_name")
+        name= request.POST.get("name")
+        text = request.POST.get("text")
+        left = request.POST.get ("left")
+        right = request.POST.get("right")
+        center = request.POST.get("center") 
+        scale_category = request.POST.get("scale_category")
         field ={
             "eventId":get_event_id(),
-            "scale_data": scale
+            "scale_data": {
+                "orientation":orientation,"numberrating":numberrating,"scalecolor":scalecolor,
+                "roundcolor":roundcolor,"fontcolor":fontcolor,"fomat":fomat,
+                "time":time,"template_name":template_name,"name":name,
+                "text":text, "left":left,"right":right,"center":center, 
+                "scale-category": scale_category}
         }
         inserted_id= dowellconnection("dowellscale","dowellscale","scale_reports","scale_reports","1094","ABCDE","insert",field)
         return JsonResponse({
